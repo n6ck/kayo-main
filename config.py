8d1b7e0a4414757ee1729333883c4588fbf574d0
nick <143164544+n6ck@users.noreply.github.com>
2023-12-06 04:26:24 -0500

    Uploaded Files

---
+++ config.py
@@ -0,0 +1,43 @@
+token = "ODc3OTg2NzI3ODYyNjE2MTM0.Gq1Kbw.7slZCieFoulFhnlkBCAkFdDhp7rxUwQQ1aSemM"
+prefix = ";"
+support = "https://discord.gg/43SkaTh5"
+owner_ids = [1148300105758298123]
+avatars_channel = 1144846050536992778
+
+
+class Network:
+    host = "0.0.0.0"
+    port = 1337
+
+
+class Database:
+    host = "127.0.0.1"
+    name = "kayo"
+    username = "rx"
+    password = "amgkN85d7eXKxJ8Cpkb6UV5b-amgkN85d7eXKxJ8Cpkb6UV5b"
+
+
+class Redis:
+    db = 0
+    host = "localhost"
+    port = 6379
+    hash = "pTc6Ajf72sHvQvFBf9w3"
+
+
+class Lavalink:
+    host = "127.0.0.1"
+    port = 2333
+    password = "6SFv2mKgMabnTpDdLQ3tPZqa"
+
+
+class Authorization:
+    Fortnite = "eb1db4b1-5f1e-4ec1-ba7c-802c3a69bfa1"
+    Lastfm = ["4210d59afeeb6c350442d7141747704c"]
+
+    class Spotify:
+        client_id = "cbad1d1ccee34d7ba772ef7e3e7798e4"
+        client_secret = "ec83198f46a34e39aac808d6adabdf62"
+
+    class Instagram:
+        session_id = "60028618709%3Ab9X6moEBSE3phh%3A1%3AAYe8GUljkWFctPMkhMOOxWHO-0BWARx90E7H42_6fkg"
+        csrf_token = "N38mRH911B6gJkePQrzHkdENYAJEhst5"
